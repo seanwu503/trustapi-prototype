9154f82af8b1763a4816dd7d98968e56f21cc1d5
Sean Wu <seanwu503@gmail.com>
2026-06-20 09:34:57 -0700

    updates

--- Trust_API/server.js
+++ Trust_API/server.js
@@ -1,32 +1,11 @@
 const express = require('express');
 const path = require('path');
 
-try {
-    if (typeof process.loadEnvFile === 'function') {
-        process.loadEnvFile(path.join(__dirname, '.env'));
-    }
-} catch (error) {
-    if (error.code !== 'ENOENT') {
-        throw error;
-    }
-}
-
 const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(204);
-    }
-
-    next();
-});
+app.use(express.static(path.join(__dirname, '..')));
 
 app.post('/check_wallet', (req, res) => {
     const wallet = req.body.wallet || '';
@@ -44,4 +23,5 @@ app.post('/check_wallet', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
+    console.log(`Open demo at http://localhost:${port}/demo.html`);
 });
